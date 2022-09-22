9d4a29d59342084cb58b51a3122be84fb93d0684
Tanya chopra <tanya3629.be20@chitkara.edu.in>
2022-09-22 09:34:40 +0530

    22sept2022

---
+++ 15sept22/class3/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+const App=()=>{
+    return(
+        <div className="App">
+            <h1>This is App component of this web site</h1>
+            <p>This is a pahragraph</p>
+        </div>
+    )
+};
+export default App;
